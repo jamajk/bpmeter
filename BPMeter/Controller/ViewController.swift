d72947968a5ea3daee9fdb66643b3b9397a81a89
jamajk <kmsk1234@o2.pl>
2020-03-17 16:41:16 +0100

    Changed the animation

--- BPMeter/Controller/ViewController.swift
+++ BPMeter/Controller/ViewController.swift
@@ -19,22 +19,18 @@ class ViewController: UIViewController {
         counter.Tap()
         bpmLabel.text = String(Int(counter.Calculate()))
         
-        let anim = UIViewPropertyAnimator(duration: 0.2, dampingRatio: 1, animations: {self.view.backgroundColor = UIColor.lightGray})
-        anim.addCompletion({_ in
-            let anim2 = UIViewPropertyAnimator(duration: 0.1, dampingRatio: 1, animations: {self.view.backgroundColor = UIColor.init(displayP3Red: 0.35, green: 0.78, blue: 0.98, alpha: 1.0)})
-            anim2.startAnimation()
-        })
-        anim.startAnimation()
+        UIView.animate(withDuration: 0.4, delay: 0, options: [.curveEaseInOut,  .allowUserInteraction], animations: {self.view.backgroundColor = UIColor.lightGray; self.view.backgroundColor = UIColor.systemTeal}, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         bpmLabel.adjustsFontSizeToFitWidth = true
         bpmLabel.text = "Start tapping"
+        view.backgroundColor = .systemTeal
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.view.bounds
-        gradientLayer.colors = [UIColor.init(displayP3Red: 0.35, green: 0.78, blue: 0.98, alpha: 1.0), UIColor.systemPurple.cgColor]
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.systemPurple.cgColor]
 
         gradientLayer.startPoint = CGPoint(x: 0, y: 0)
         gradientLayer.endPoint = CGPoint(x: 0, y: 1)
