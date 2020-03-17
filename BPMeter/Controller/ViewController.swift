0dd62030f39812e0c74c4b89cc69aad9754c2ceb
jamajk <kmsk1234@o2.pl>
2020-03-17 15:56:18 +0100

    Added a nicer gradient and an attempt at a touch animation

--- BPMeter/Controller/ViewController.swift
+++ BPMeter/Controller/ViewController.swift
@@ -17,7 +17,14 @@ class ViewController: UIViewController {
     
     @objc func mainTap(sender: UITapGestureRecognizer) {
         counter.Tap()
-        bpmLabel.text = String(Int(counter.Calculate()))        
+        bpmLabel.text = String(Int(counter.Calculate()))
+        
+        let anim = UIViewPropertyAnimator(duration: 0.2, dampingRatio: 1, animations: {self.view.backgroundColor = UIColor.lightGray})
+        anim.addCompletion({_ in
+            let anim2 = UIViewPropertyAnimator(duration: 0.1, dampingRatio: 1, animations: {self.view.backgroundColor = UIColor.init(displayP3Red: 0.35, green: 0.78, blue: 0.98, alpha: 1.0)})
+            anim2.startAnimation()
+        })
+        anim.startAnimation()
     }
     
     override func viewDidLoad() {
@@ -27,13 +34,13 @@ class ViewController: UIViewController {
         
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.view.bounds
-        gradientLayer.colors = [UIColor.blue.cgColor, UIColor.purple.cgColor]
-        print(gradientLayer.startPoint)
-        print(gradientLayer.endPoint)
-        
+        gradientLayer.colors = [UIColor.init(displayP3Red: 0.35, green: 0.78, blue: 0.98, alpha: 1.0), UIColor.systemPurple.cgColor]
+
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
         
         self.view.layer.insertSublayer(gradientLayer, at: 0)
-        
+
         let tap = UITapGestureRecognizer(target: self, action: #selector(mainTap(sender:)))
         view.addGestureRecognizer(tap)
     }
