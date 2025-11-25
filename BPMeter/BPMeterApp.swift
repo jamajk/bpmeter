743995f2408f4f3490444ec9052d08770a0c4222
Jakub Majkowski <kmsk1234@o2.pl>
2025-11-26 00:32:40 +0100

    Apple told me to update the app so here's an initial commit for a rewrite because why not

---
+++ BPMeter/BPMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPMeterApp.swift
+//  BPMeter
+//
+//  Created by Jakub Majkowski on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BPMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TapTempoView() // TODO: Change
+        }
+    }
+}
---
+++ BPMeter/Views/MetronomeView.swift
@@ -0,0 +1,14 @@
+//
+//  MetronomeView.swift
+//  BPMeter
+//
+//  Created by Jakub Majkowski on 25/11/2025.
+//
+
+import SwiftUI
+
+struct MetronomeView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
