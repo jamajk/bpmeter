882daf1e9e0522a1d99282cbc9997f2a468888d9
Jakub Majkowski <kmsk1234@o2.pl>
2025-12-02 21:36:17 +0100

    Add animation for text

--- BPMeter/Views/TapTempoView.swift
+++ BPMeter/Views/TapTempoView.swift
@@ -20,38 +20,14 @@ struct TapTempoView: View {
 
     var body: some View {
         ZStack {
-            Rectangle()
-                .fill(viewModel.background.backgroundColor.gradient)
-                .edgesIgnoringSafeArea(.all)
-                .background(
-                    GeometryReader { reader in
-                        Color.clear
-                            .onAppear { viewModel.updateViewportSize(reader.size) }
-                            .onChange(of: reader.size) { _, newSize in
-                                viewModel.updateViewportSize(newSize)
-                            }
-                    }
-                )
-                .rippleEffect(
-                    at: rippleOrigin,
-                    trigger: rippleTrigger,
-                    configuration: .init(
-                        duration: 1.5,
-                        amplitude: 30,
-                        frequency: 12,
-                        decay: 12,
-                        speed: 1500
-                    )
-                )
+            background
 
             Circle()
-                .fill(Color.red)
+                .fill(Color.white.opacity(0.5))
                 .frame(width: 120)
                 .position(x: viewModel.bubbleXPosition, y: viewModel.bubbleYPosition)
 
-            Rectangle()
-                .fill(.ultraThinMaterial)
-                .edgesIgnoringSafeArea(.all)
+            blurLayer
 
             content
         }
@@ -69,16 +45,59 @@ struct TapTempoView: View {
         .onDisappear { viewModel.onDisappear() }
     }
 
+    private var background: some View {
+        Rectangle()
+            .fill(viewModel.background.backgroundColor.gradient)
+            .edgesIgnoringSafeArea(.all)
+            .background(
+                GeometryReader { reader in
+                    Color.clear // TODO: Bugs out when switching to metronome and back
+                        .onAppear { viewModel.updateViewportSize(reader.size) }
+                        .onChange(of: reader.size) { _, newSize in
+                            viewModel.updateViewportSize(newSize)
+                        }
+                }
+            )
+            .rippleEffect(
+                at: rippleOrigin,
+                trigger: rippleTrigger,
+                configuration: .init(
+                    duration: 1.5,
+                    amplitude: 30,
+                    frequency: 12,
+                    decay: 12,
+                    speed: 1500
+                )
+            )
+    }
+
     private var content: some View {
-        Text(labelText)
-            .font(.largeTitle)
-            .bold()
-            .foregroundStyle(.white)
+        VStack(spacing: 8) {
+            Text(labelText)
+                .font(.largeTitle)
+                .bold()
+                .foregroundStyle(.white)
+                .contentTransition(.numericText())
+                .animation(.default, value: labelText)
+
+            if viewModel.isBPMDetected {
+                Text("[beats per minute]")
+                    .font(.body)
+                    .foregroundStyle(.white)
+            }
+        }
+        .animation(.default, value: viewModel.isBPMDetected)
+    }
+
+    private var blurLayer: some View {
+        Rectangle()
+            .fill(.ultraThinMaterial)
+            .edgesIgnoringSafeArea(.all)
     }
 
     private var labelText: String {
-        guard viewModel.bpmValue != .zero else {
-            return "Start tapping" // TODO: Localize
+        guard viewModel.isBPMDetected else {
+            return "start tapping" // TODO: Localize
         }
         return String(viewModel.bpmValue)
     }
