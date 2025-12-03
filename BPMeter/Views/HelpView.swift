b41380a84fc20969d1dc189529b4c3d9512a43b3
Jakub Majkowski <kmsk1234@o2.pl>
2025-12-03 22:10:12 +0100

    WIP add settings and credits

--- BPMeter/Views/HelpView.swift
+++ BPMeter/Views/HelpView.swift
@@ -22,12 +22,12 @@ struct HelpView: View {
                     .overlay(Text("How to metronome"))
 
                 NavigationLink(
-                    destination: { Text("Lorem ipsum") },
+                    destination: { SettingsView() },
                     label: { Text("Settings").font(BPFont.lexendLightBody) }
                 )
 
                 NavigationLink(
-                    destination: { Text("Lorem ipsum") },
+                    destination: { LicenceListView() },
                     label: { Text("Software credits").font(BPFont.lexendLightBody) }
                 )
             }
@@ -49,5 +49,49 @@ struct HelpView: View {
 
     private var divider: some View {
         Divider().padding(.vertical, 8)
+    }
+}
+
+struct LicenceListView: View {
+    var body: some View {
+        List {
+            NavigationLink(
+                destination: { Text("Lorem ipsum") },
+                label: { Text("SwiftUIRippleEffect").font(BPFont.lexendLightBody) }
+            )
+
+            NavigationLink(
+                destination: { Text("Lorem ipsum") },
+                label: { Text("Lexend").font(BPFont.lexendLightBody) }
+            )
+        }
+        .toolbar {
+            ToolbarItem(placement: .title) {
+                Text("credits")
+                    .font(BPFont.lexendThinLargeTitle)
+            }
+        }
+    }
+}
+
+struct SettingsView: View {
+    @State var isSoundOn: Bool = true // TODO: Placeholder
+    @State var isHapticFeedbackOn: Bool = true // TODO: Placeholder
+    var body: some View {
+        List {
+            Toggle(isOn: $isSoundOn) {
+                Text("Sounds").font(BPFont.lexendLightBody)
+            }
+
+            Toggle(isOn: $isHapticFeedbackOn) {
+                Text("Haptic feedback").font(BPFont.lexendLightBody)
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .title) {
+                Text("settings")
+                    .font(BPFont.lexendThinLargeTitle)
+            }
+        }
     }
 }
